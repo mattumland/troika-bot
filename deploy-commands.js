d2cb6945c93444c0e938f15793584e513e9a8daa
Matt Umland <mattumland@gmail.com>
2023-04-06 11:42:22 -0400

    Add slash commands and deploy script

---
+++ deploy-commands.js
@@ -0,0 +1,37 @@
+const { REST, Routes } = require('discord.js');
+const { clientId, guildId, token } = require('./config.json');
+const fs = require('node:fs');
+const path = require('node:path');
+
+const commands = [];
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+	for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    commands.push(command.data.toJSON());
+  }
+}
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+(async () => {
+  try {
+    console.log(`Refreshing ${commands.length} (/) commands.`);
+    
+    const data = await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: commands },
+		);
+
+    console.log(`Successfully reloaded ${data.length} (/) commands.`);
+  } catch (error) {
+      console.error(error);
+  }
+})();
