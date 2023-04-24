db5fe5fca0b3b35c77de240d38a3c93ba4fc051e
Matt Umland <mattumland@gmail.com>
2023-04-24 13:04:41 -0400

    Add better formatting

--- commands/utility/gameInfo.js
+++ commands/utility/gameInfo.js
@@ -6,8 +6,8 @@ module.exports = {
     .setDescription('Returns the contents of the game object'),
   async execute(interaction) {
     const pcs = global.game.displayPcs();
-    const defaultStack = global.game.defaultStack;
-    const currentStack = global.game.currentStack;
+    const defaultStack = global.game.default.StackreplaceAll(',', ', ');
+    const currentStack = global.game.currentStack.StackreplaceAll(',', ', ');
     const currentTurn = global.game.currentTurn;
     await interaction.reply(`PCs: ${pcs}\ndefaultStack: ${defaultStack}\ncurrentStack: ${currentStack}\ncurrentTurn: ${currentTurn}`);
   },  
