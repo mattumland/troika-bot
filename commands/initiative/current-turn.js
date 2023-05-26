5e491cd81d86db171810f841e82625aebf9818a7
Matt Umland <mattumland@gmail.com>
2023-05-26 15:11:56 -0400

    Add embeds to current-turn

--- commands/initiative/current-turn.js
+++ commands/initiative/current-turn.js
@@ -1,11 +1,20 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('current-turn')
 		.setDescription('Replies with the current turn'),
 	async execute(interaction) {
-    const currentTurn = global.game.currentTurn || 'No one is currently acting. Type `build-stack` to set up an initiative stack.'
-		await interaction.reply(`${currentTurn}`);
+    const currentTurn = `It is ${global.game.currentTurn}'s turn` || 'No one is currently acting. Type `build-stack` to set up an initiative stack.'
+		const turnEmbed = new EmbedBuilder()
+      .setTitle(currentTurn)
+      if (global.game.currentTurn=== 'Enemy') {
+        turnEmbed.setColor('Red')
+      } else if (global.game.currentTurn === 'Henchman') {
+        turnEmbed.setColor('Green')
+      } else {
+        turnEmbed.setColor('Blurple')
+      }
+		await interaction.reply({ embeds: [turnEmbed] });
 	},
 };
